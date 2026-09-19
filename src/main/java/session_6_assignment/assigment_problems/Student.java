b647ac85c5496eae8f75c6a2e3502a3d84a9a316
Shivansh Bansal <shivansh556677@gmail.com>
2026-09-19 11:07:25 +0530

    Add Session 6 class and assignment problems

---
+++ src/main/java/session_6_assignment/assigment_problems/Student.java
@@ -0,0 +1,26 @@
+package session_6_assignment.assigment_problems;
+
+public class Student {
+    String name;
+    int attendance;
+    static String collegeName = "SRM Institute of Science and Technology";
+    static int studentCount = 0;
+
+    public Student(String name, int attendance) {
+        this.name = name;
+        this.attendance = attendance;
+        studentCount++;
+    }
+
+    public static void printCollegeInfo() {
+        System.out.println(collegeName);
+        System.out.println("Students created: " + studentCount);
+    }
+
+    public static void main(String[] args) {
+        new Student("Rahul", 85);
+        new Student("Pooja", 92);
+
+        Student.printCollegeInfo();
+    }
+}
